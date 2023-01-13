7ca4034c9f1b2d4f47ac40bb2d5176a4887603a0
scupdev <ot.gaming132@gmail.com>
2023-01-13 07:14:12 +0530

    created project

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import {createSpinner} from "nanospinner";
+
+console.log("hello mother.")
