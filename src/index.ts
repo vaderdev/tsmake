c7c8ce6d3a57012bbf74e33704d8abb9e45563d7
scupdev <ot.gaming132@gmail.com>
2023-01-22 13:19:26 +0530

    got package.json generating

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import gradient from "gradient-string";
 import { createSpinner } from "nanospinner";
 import title from "./title.js";
-import { name_of_project } from "./prompt.js";
+import { create_project } from "./prompt.js";
 
 await title();
-name_of_project();
+create_project();
--- src/prompt.ts
+++ src/prompt.ts
@@ -1,10 +1,13 @@
 import inquirer from "inquirer";
 import * as fs from "fs";
-import * as tsConfigJSON from "./config/tsconfig.template.json" assert { type: "json" };
+import * as ts_config_json from "./config/tsconfig.template.json" assert { type: "json" };
+import * as package_json_cli from "./config/package_json/package.cli.json" assert { type: "json" };
+import * as package_json_api from "./config/package_json/package.api.json" assert { type: "json" };
+import * as package_json_barebone from "./config/package_json/package.barebone.json" assert { type: "json" };
 
 let project_dir = "";
 
-export function name_of_project() {
+export function create_project() {
   inquirer
     .prompt([{ type: "input", message: "Name Of Project >", name: "name" }])
     .then((answers) => {
@@ -15,16 +18,42 @@ export function name_of_project() {
         fs.mkdirSync(dir);
       }
 
-      fs.writeFile(project_dir + "/tsconfig.json", JSON.stringify(tsConfigJSON), function (err) {
-        if (err) {
-          return console.log(err);
+      fs.writeFile(
+        project_dir + "/tsconfig.json",
+        JSON.stringify(ts_config_json),
+        function (err) {
+          if (err) {
+            return console.log(err);
+          }
         }
-      });
+      );
+
+      if (project_dir != "") {
+        packages_required();
+      }
     });
 }
 
-export function packages_required() {
-    inquirer
-        .prompt([{}])
-        .then((answers) => {})
+function packages_required() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Type Of Project",
+        name: "type",
+        choices: ["CLI", "API", "Blank"],
+      },
+    ])
+    .then(({ type }) => {
+      let package_json = "";
+      if (type === "CLI") {
+        package_json = JSON.stringify(package_json_cli);
+      } else if (type === "API") {
+        package_json = JSON.stringify(package_json_api);
+      } else {
+        package_json = JSON.stringify(package_json_barebone);
+      }
+      
+      fs.writeFileSync(project_dir + "/package.json", package_json);
+    });
 }
