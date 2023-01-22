d37c4302405d4edacaaebc27ce8c4debdefe0a49
scupdev <ot.gaming132@gmail.com>
2023-01-22 14:57:30 +0530

    removing chalk animations for the time being since its causing some errors

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,2 @@
-#!/usr/bin/env node
-import title from "./title.js";
 import { create_project } from "./prompt.js";
-await title();
 create_project();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { createSpinner } from "nanospinner";
-import title from "./title.js";
 import { create_project } from "./prompt.js";
 
-await title();
 create_project();
